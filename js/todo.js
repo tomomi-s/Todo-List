c6fa8886ac14df3bec5fb928c61d065012179dc5
Tomomi Suda <tomomisuda@Tomomi-no-MacBook-Air.local>
2017-10-27 19:44:06 +0200

    upload all files

---
+++ js/todo.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".fa-plus").on("click", function(){
+	$("#input").slideToggle();
+	});
+
+	$("input").keypress(function(event){
+		// var newTodos = [newList];
+		if(event.which === 13){
+			var newTodo = $("input").val();
+			var newList = '<li class="list"><div class="trashIcon"><i class="fa fa-trash" aria-hidden="true"></i></div><div class="todo">' + newTodo + '</div></li>';
+			$("#lists").append(newList);
+			$('input').val('');
+		}
+
+		$("#lists li:odd").addClass('todo-odd');
+		$("#lists li:even").addClass('todo-even');
+	});
+
+	$(document).on("mouseenter", ".list", function(){
+		// $(".trashIcon", this).show();
+		$(".trashIcon", this).addClass("showTrashIcon");
+		// $(".trashIcon", this).animate({ left: '0' }, 500); 
+	});
+
+	$(document).on("mouseleave", ".list", function(){
+		$(".trashIcon", this).removeClass("showTrashIcon");
+		// $(".trashIcon", this).animate({ left: '-32px' }, 500); 
+		// $(".trashIcon", this).hide();
+	});
+
+	$(document).on("click", ".trashIcon", function(){
+		$(this).parent().remove();
+		$("#lists li:odd").removeClass('todo-even').addClass('todo-odd');
+		$("#lists li:even").removeClass('todo-odd').addClass('todo-even');
+	});
+
+	$(document).on("click", ".todo", function(){
+		$(this).toggleClass("textBorder");
+	});
+
+});
